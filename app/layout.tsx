a0148fb524bafe32dc820c3bf724c10da690a600
iverbitskyi <ivan.verbitskyj@gmail.com>
2024-03-03 20:50:19 +0200

    feat: creating auth & spinner loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import { Inter } from "next/font/google";
-import type { Metadata } from "next";
-
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import "./globals.css";
+
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,15 +30,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body className={inter.className}>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
-					storageKey="notekeep-theme-2"
-				>
-					{children}
-				</ThemeProvider>
+				<ConvexClientProvider>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+						storageKey="notekeep-theme-2"
+					>
+						{children}
+					</ThemeProvider>
+				</ConvexClientProvider>
 			</body>
 		</html>
 	);
